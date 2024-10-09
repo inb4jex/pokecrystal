96cbf165c46b1a56ccbeb69e31fa285c4121a2ec
Jexams <jarceo515@gmail.com>
2024-10-08 21:59:21 -0700

    Trying to implement running

--- constants/sprite_constants.asm
+++ constants/sprite_constants.asm
@@ -104,6 +104,8 @@
 	const SPRITE_ENTEI ; 64
 	const SPRITE_RAIKOU ; 65
 	const SPRITE_STANDING_YOUNGSTER ; 66
+	const SPRITE_CHRIS_RUN ; 67 (adding new constants)
+	const SPRITE_KRIS_RUN ; 68 (adding new constants)
 DEF NUM_OVERWORLD_SPRITES EQU const_value - 1
 
 ; SpriteMons indexes (see data/sprites/sprite_mons.asm)
--- engine/overworld/player_movement.asm
+++ engine/overworld/player_movement.asm
@@ -280,7 +280,7 @@ DoPlayerMovement::
 
 ; Downhill riding is slower when not moving down.
 	call .BikeCheck
-	jr nz, .walk
+	jr nz, .HandleWalkAndRun ;only checks transition when not on bike
 
 	ld hl, wBikeFlags
 	bit BIKEFLAGS_DOWNHILL_F, [hl]
@@ -320,6 +320,26 @@ DoPlayerMovement::
 .bump
 	xor a
 	ret
+
+;added to call player state changes
+.HandleWalkAndRun
+	ld a, [wWalkingDirection]
+	cp STANDING
+	jr z, .ensurewalk
+	ldh a, [hJoypadDown]
+	and B_BUTTON
+	cp B_BUTTON
+	jr nz, .ensurewalk
+	ld a, [wPlayerState]
+	cp PLAYER_RUN
+	call nz, .StartRunning
+	jr .fast
+
+.ensurewalk
+	ld a, [wPlayerState]
+	cp PLAYER_NORMAL
+	call nz, .StartWalking
+	jr .walk
 
 .TrySurf:
 	call .CheckSurfPerms
@@ -787,6 +807,23 @@ ENDM
 	pop bc
 	ret
 
+;Update sprite for running
+.StartRunning:
+	push bc
+	ld a, PLAYER_RUN
+	ld [wPlayerState], a
+	call UpdatePlayerSprite
+	pop bc
+	ret
+
+;Update sprite for walking
+.StartWalking:
+	push bc
+	ld a, PLAYER_NORMAL
+	ld [wPlayerState], a
+	call UpdatePlayerSprite
+	ret
+	
 CheckStandingOnIce::
 	ld a, [wPlayerTurningDirection]
 	cp 0
